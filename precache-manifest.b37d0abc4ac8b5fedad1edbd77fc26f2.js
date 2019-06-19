e37b4c4f396a2a2bc32e505b8aec0cb934dfab09
KongYue <haozaici@gmail.com>
2019-06-19 23:27:40 +1000

    Updates

---
+++ precache-manifest.b37d0abc4ac8b5fedad1edbd77fc26f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "8c7ef7db2bd348c499e8",
+    "url": "/course-directory/static/js/main.8c7ef7db.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/course-directory/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "8c7ef7db2bd348c499e8",
+    "url": "/course-directory/static/css/main.4c916034.chunk.css"
+  },
+  {
+    "revision": "5f16ef6a9724aaf073172a975fa92290",
+    "url": "/course-directory/index.html"
+  }
+];
